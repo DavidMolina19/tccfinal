9b47ebcc93a4c270954e08bf03d15cf2efec2e4d
409 <409@academicos.cesde.edu.co>
2022-10-10 14:40:52 -0500

    ok

---
+++ src/app/grafica/grafica.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import {Color,ScaleType,LegendPosition} from '@swimlane/ngx-charts'
+
+
+@Component({
+  selector: 'app-grafica',
+  templateUrl: './grafica.component.html',
+  styleUrls: ['./grafica.component.css']
+})
+export class GraficaComponent   {
+
+
+   single = [
+  {
+    "name": "Germany",
+    "value": 8940000
+  },
+  {
+    "name": "USA",
+    "value": 5000000
+  },
+  {
+    "name": "France",
+    "value": 7200000
+  },
+    {
+    "name": "UK",
+    "value": 6200000
+  }
+];
+  view: [number,number] = [700, 400];
+
+  // options
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+  legendPosition: LegendPosition=LegendPosition.Below;
+
+  colorScheme : Color = {
+    name:'colorestcc',
+    selectable:true,
+    group: ScaleType.Ordinal,
+    domain: ['#506E11', '#2388DC', '#8D99A3', '#EFF3F6']
+  };
+
+  constructor() {
+    // Object.assign(this, { single });
+  }
+
+  onSelect(data:any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data:any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data:any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+}
