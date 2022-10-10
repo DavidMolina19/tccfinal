374dee68799ab188878fff306bbdc514edb0af83
206 <206@academicos.cesde.edu.co>
2022-10-10 12:11:10 -0500

    10/10/2022

--- src/app/formularioregistro/formularioregistro.component.ts
+++ src/app/formularioregistro/formularioregistro.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormBuilder,Validators} from '@angular/forms'
+import { MercanciasService } from '../services/mercancias.service';
+import { ZonasService } from '../services/zonas.service';
 
 @Component({
   selector: 'app-formularioregistro',
@@ -10,11 +12,28 @@ export class FormularioregistroComponent implements OnInit {
 
   formulario!:FormGroup;
 
-  constructor(public fabricaDiccionario:FormBuilder) { }
+  controlDeZona:boolean=true;
+
+  datosZonas:any[]=[];
+
+  constructor(
+    public fabricaDiccionario:FormBuilder,
+    public servicioMercancias:MercanciasService,
+    public servicioZonas:ZonasService
+    ) { }
 
   ngOnInit(): void {
+    
 
     this.formulario=this.inicializarFormulario()
+
+    this.servicioZonas.consultarZonas()
+    .subscribe(respuesta=>{
+          this.datosZonas=respuesta.map((zona:any)=>{
+            return {nombre:zona.nombre}
+          })
+    })
+    console.log(this.datosZonas)
   }
 
   public   analizarFormulario():void{
@@ -23,19 +42,19 @@ export class FormularioregistroComponent implements OnInit {
 
   public inicializarFormulario():FormGroup{
    return this.fabricaDiccionario.group({
-    iup:['david',[Validators.required,Validators.minLength(6)]],
-    tiporemitente:['empresa',[Validators.required]],
-    idremitente:['12345',[Validators.required]],
-    nombreremitente:['camila',[Validators.required]],
-    departamentoremitente:['antioquia',[Validators.required]],
-    municipioremitente:['guarne',[Validators.required]],
-    direccionremitente:['cr50#52-51',[Validators.required]],
-    tipodestinatario:['persona',[Validators.required]],
-    iddestinatario:['145789',[Validators.required]],
-    nombredestinatario:['valentina',[Validators.required]],
-    departamentodestinatario:['valle',[Validators.required]],
-    municipiodestinatario:['cali',[Validators.required]],
-    direcciondestinatario:['calle 40 #50-30',[Validators.required]],
+    iup:['',[Validators.required]],
+    tiporemitente:['',[Validators.required]],
+    idremitente:['',[Validators.required]],
+    nombreremitente:['',[Validators.required]],
+    departamentoremitente:['',[Validators.required]],
+    municipioremitente:['',[Validators.required]],
+    direccionremitente:['',[Validators.required]],
+    tipodestinatario:['',[Validators.required]],
+    iddestinatario:['',[Validators.required]],
+    nombredestinatario:['',[Validators.required]],
+    departamentodestinatario:['',[Validators.required]],
+    municipiodestinatario:['',[Validators.required]],
+    direcciondestinatario:['',[Validators.required]],
 
 
 
@@ -44,6 +63,42 @@ export class FormularioregistroComponent implements OnInit {
   })
    
             
+  }
+
+  public buscarMercancia(){
+
+    let iup=this.formulario.value.iup
+    this.servicioMercancias.buscarMercanciaPorID(iup)
+    .subscribe(
+      respuesta=>{
+      this.formulario.patchValue({
+        tiporemitente:respuesta.tipoRemitente,
+        idremitente:respuesta.idRemitente,
+        nombreremitente:respuesta.nombreRemitente,
+        departamentoremitente:respuesta.departamentoRemitente,
+        municipioremitente:respuesta.municipioRemitente,
+        direccionremitente:respuesta.direccionRemitente,
+
+        tipodestinatario:respuesta.tipoDestinatario,
+        iddestinatario:respuesta.idDestinatario,
+        nombredestinatario:respuesta.nombreDestinatario,
+        departamentodestinatario:respuesta.departamentoDestinatario,
+        municipiodestinatario:respuesta.municipioDestinatario,
+        direcciondestinatario:respuesta.direccionDestinatario
+      })
+      this.formulario.disable()
+      this.formulario.controls['iup'].enable()
+      this.controlDeZona=false
+    },
+      
+
+      error=>{console.log(error.error)
+      this.formulario.enable()
+      this.formulario=this.inicializarFormulario()
+      this.controlDeZona=true
+
+      }
+    )
   }
 
 }
